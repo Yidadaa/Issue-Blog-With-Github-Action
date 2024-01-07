21b4bcfc5f68ed16482d3f09450b704c4f7647c5
LianHua <hwaltz@qq.com>
2024-01-07 10:43:34 +0800

    api

--- src/.vuepress/custom.js
+++ src/.vuepress/custom.js
@@ -1,79 +1,80 @@
 /**
  * @file: custom config
  */
-
 let mainConfig = {
-  vssueConfig: {
-    owner: "LianHua",
-    repo: "lianhua.github.io",
-    clientId: "4b03b15c993ac0c75319",
-    clientSecret: "b20044180530d2b17d01729ff2aeb55e0df903c5"
-  },
-
-  repoConfig: {
-    owner: "LianHua",
-    repo: "lianhua.github.io",
-    pushBranch: "master",
-    email: "hwaltz@qq.com",
-    filterUsers: ['LianHua']
-  },
-
-  title: "似水年华",
-  description: "在这里,我的一切，热爱永不停歇。",
-  customDomain: "",
-  base: "/",
-
-  slogan: {
-    main: "见天地",
-    sub: "观众生，寻吾身"
-  },
-
-  themeConfig: {
-    nav: [
-      {
-        name: "首页",
-        link: "/"
-      },
-      {
-        name: "Github",
-        link: "https://github.com/LianHua"
-      },
-      {
-        name: "关于我",
-        link: "/cv.html"
-      }
-    ],
-    headTitle: ["https://api.vvhan.com/api/ian"],
-    friendLinks: [
-      {
-        name: "watercow",
-        link: "https://github.com/watercow/watercow.github.io/issues"
-      },
-      {
-        name: "ycjgg",
-        link: "https://ycjgg.github.io"
-      }
-    ],
-    extraFooters: [
-      {
-        title: "关于",
-        text: "@ 陕 ICP 备 18000508 号 - 1",
-        link: "https://beian.miit.gov.cn/"
-      }
-    ],
-    pageCount: true
-  },
-
-  head: [
-    [
-      "link",
-      {
-        rel: "icon",
-        type: "image/png",
-        href: "https://avatars2.githubusercontent.com/u/16968934?s=460&v=4"
-      }
+    vssueConfig: {
+        owner: "LianHua",
+        repo: "lianhua.github.io",
+        clientId: "4b03b15c993ac0c75319",
+        clientSecret: "b20044180530d2b17d01729ff2aeb55e0df903c5"
+    },
+    
+    repoConfig: {
+        owner: "LianHua",
+        repo: "lianhua.github.io",
+        pushBranch: "master",
+        email: "hwaltz@qq.com",
+        filterUsers: ['LianHua']
+    },
+    
+    title: "似水年华",
+    description: "在这里,我的一切，热爱永不停歇。",
+    customDomain: "",
+    base: "/",
+    
+    slogan: {
+        main: "见天地",
+        sub: "观众生，寻吾身"
+    },
+    
+    themeConfig: {
+        nav: [
+            {
+                name: "首页",
+                link: "/"
+            },
+            {
+                name: "Github",
+                link: "https://github.com/LianHua"
+            },
+            {
+                name: "关于我",
+                link: "/cv.html"
+            }
+        ],
+        
+        // 修改了 headtitle 为调用 https://api.vvhan.com/api/ian
+        headTitle: <script>fetch('https://api.vvhan.com/api/ian').then(response => response.text()).then((data) => document.write(`${JSON.parse(data)[Math.floor(Math.random()*JSON.parse(data).length)]}`));</script>,
+        friendLinks: [
+            {
+                name: "watercow",
+                link: "https://github.com/watercow/watercow.github.io/issues"
+            },
+            {
+                name: "ycjgg",
+                link: "https://ycjgg.github.io"
+            }
+        ],
+        extraFooters: [
+            {
+                title: "关于",
+                text: "@ 陕 ICP 备 18000508 号 - 1",
+                link: "https://beian.miit.gov.cn/"
+            }
+        ],
+        pageCount: true
+    },
+    
+    head: [
+        [
+            "link",
+            {
+                rel: "icon",
+                type: "image/png",
+                href: "https://avatars2.githubusercontent.com/u/16968934?s=460&v=4"
+            }
+        ]
     ]
-  ]
 }
-
 module.exports = mainConfig
+ 
